a58f1a784e89161503c14a31c955af10f7291b27
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2026-09-16 16:29:47 +0200

    published books

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@ git pull
 
 rm -rf manuscript
 mdkir manuscript
+mdkir manuscript/resources
+cp title_page.jpg manuscript/resources
 
 cp -r /Users/zeljkoobrenovic/PycharmProjects/spec-driven-journals/manuscripts/owned manuscript
 
